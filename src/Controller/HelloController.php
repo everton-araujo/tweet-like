b0dd8e28d134b630c4741fd7f0bfd3fd39f70b05
Everton <everton.aaraujo@outlook.com>
2023-02-21 07:34:42 -0300

    First tests

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/', name: 'app_')]
+class HelloController extends AbstractController
+{
+    private array $messages = [
+        ['message' => 'Hello', 'created' => '2022/06/12'],
+        ['message' => 'Hi', 'created' => '2022/04/12'],
+        ['message' => 'Bye!', 'created' => '2021/05/12']
+    ];
+
+    #[Route('/{limit<\d+>?3}', name: 'index')]
+    public function index(int $limit): Response
+    {
+        return $this->render(
+            'hello/index.html.twig',
+            [
+                'messages' => $this->messages,
+                'limit' => $limit
+            ]
+        );
+    }
+
+    #[Route('/messages/{id<\d+>}', name: 'show_one')]
+    public function showOne(int $id): Response
+    {
+        return $this->render(
+            'hello/show_one.html.twig',
+            [
+                'message' => $this->messages[$id]
+            ]
+        );
+    }
+}
