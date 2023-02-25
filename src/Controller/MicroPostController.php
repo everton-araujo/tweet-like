ef8bf633548c28b49edf06754de801314ed7d0bf
Everton <everton.aaraujo@outlook.com>
2023-02-25 14:51:38 -0300

    First controllers steps

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -2,17 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\MicroPost;
+use App\Repository\MicroPostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/micro-post', name: 'app_micro_post_')]
 class MicroPostController extends AbstractController
 {
-    #[Route('/micro/post', name: 'app_micro_post')]
+    public function __construct(private MicroPostRepository $microPostRepository)
+    {
+    }
+
+    #[Route('/', name: 'show')]
     public function index(): Response
     {
         return $this->render('micro_post/index.html.twig', [
-            'controller_name' => 'MicroPostController',
+            'posts' => $this->microPostRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/{post}', name: 'show_one')]
+    public function showOne(MicroPost $post): Response
+    {
+        return $this->render('micro_post/show.html.twig', [
+            'post' => $post,
         ]);
     }
 }
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\MicroPost;
+use DateTime;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $microPost1 = new MicroPost();
+        $microPost1->setTitle('Welcome to 1');
+        $microPost1->setText('Welcome to 1');
+        $microPost1->setCreated(new DateTime());
+        $manager->persist($microPost1);
+
+        $microPost2 = new MicroPost();
+        $microPost2->setTitle('Welcome to 2');
+        $microPost2->setText('Welcome to 2');
+        $microPost2->setCreated(new DateTime());
+        $manager->persist($microPost2);
+
+        $microPost3 = new MicroPost();
+        $microPost3->setTitle('Welcome to 3');
+        $microPost3->setText('Welcome to 3');
+        $microPost3->setCreated(new DateTime());
+        $manager->persist($microPost3);
+
+        $manager->flush();
+    }
+}
